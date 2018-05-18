e9719a8e81bf193fb25fe2f5c03c401424922612
bennywj <benny.wijaya8@yahoo.com>
2018-05-18 18:59:06 +0700

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Cinema-XXII-Tugas
+untuk menjalankan server cd ke directory /server dan jalankan npm start
+untuk menjalankan client cd ke directory /client dan jalankan npm start
--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    //baseURL: 'https://cinema-xxii-server.herokuapp.com/'
     baseURL: 'http://localhost:8081/'
   })
 }
